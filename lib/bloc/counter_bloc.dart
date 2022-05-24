7a676ccb3ee1eb2a6b7ef8a7f4036416e78efe66
lazar <ljovicic00@gmail.com>
2022-05-24 06:19:11 +0200

    added small documentation

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -8,11 +8,21 @@ class CounterBloc {
   CounterBloc() {
     _counterEventController.stream.listen(mapEventToState);
   }
+
+  ///Controller used to send a new States to the view.
   final _counterStateController = StreamController<CounterState>();
+
+  ///The StreamSink is used internally to pass a new values/states so the view.
   StreamSink<CounterState> get _inCounter => _counterStateController.sink;
+
+  ///The Stream is used to expose/send a new states to the view,
+  ///view can subscribe/listen for a new states using this.
   Stream<CounterState> get counter => _counterStateController.stream;
 
+  ///Controller used to listen for events.
   final _counterEventController = StreamController<CounterEvent>();
+
+  ///Used to listen for a new events.
   StreamSink<CounterEvent> get counterEventSink => _counterEventController.sink;
 
   void mapEventToState(CounterEvent event) {
